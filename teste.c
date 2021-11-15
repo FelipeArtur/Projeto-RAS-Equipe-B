044e116a0f53c96975b7d9f97ba617964d5c6dbe
Felipe Artur Macedo Lima <xpxlipe@gmail.com>
2021-11-15 18:13:50 -0300

    Delete teste.c

--- teste.c
+++
@@ -1,7 +0,0 @@
-FDIPGJSDFJPOGD´FOGJO´PSDFJKGO´PKDSFOP´GJDFOPSJGP[IOSJP[OFSJDPOKASÓDFKAS´
-                                                           FK´PSD
-                                                               floatASDFA
-                                                                   __STDC_UTF_16__SD
-                                                                       _USE_DECLSPECS_FOR_SALFA
-                                                                           __STDC_HOSTED__F
-                                                                               __STDC_UTF_16__ASD float]]
