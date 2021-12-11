84d07a076159cd1d528dbb0409f6399f26a6dd4c
Felipe Artur Macedo Lima <xpxlipe@gmail.com>
2021-12-11 02:54:41 -0300

    Update code.ino
    Versão atual

--- Code/code.ino
+++ Code/code.ino
@@ -1,45 +1,39 @@
 //Sensor setup
-#define echoPin1 10
-#define trigPin1 11
-#define echoPin2 8
-#define trigPin2 9
-#define echoPin3 6
-#define trigPin3 7
+#define echoPin1 10 //Back
+#define trigPin1 11 //Back
+#define echoPin2 8 //Right
+#define trigPin2 9 //Right
+#define echoPin3 6 //Left
+#define trigPin3 7 //Left
 
 //Variables declaration
 int motor = 12;
 int piezo = 13;
-int greenled_back = 4;
-int yellowled_back = 3;
-int redled_back = 2;
 int duration = 0;
 int back_distance = 0;
 int right_distance = 0;
 int left_distance = 0;
+int RGB1 = 2;
+int RGB2 = 3;
+int RGB3 = 4;
 
 // Startup configs for Arduino
 void setup()
 {
   pinMode(piezo, OUTPUT);
   pinMode(motor, OUTPUT);
-  pinMode(greenled_back , OUTPUT);
-  pinMode(yellowled_back , OUTPUT);
-  pinMode(redled_back, OUTPUT);
-
-  //Leds for left and right sensors
-  pinMode(A0, OUTPUT); //greenled_left
-  pinMode(A1, OUTPUT); //yellowled_left
-  pinMode(A2, OUTPUT); //redled_left
-  pinMode(A3, OUTPUT); //greenled_right
-  pinMode(A4, OUTPUT); //yellowled_right
-  pinMode(A5, OUTPUT); //redled_right
+  pinMode (RGB1, OUTPUT);
+  pinMode (RGB2, OUTPUT);
+  pinMode (RGB3, OUTPUT);
 
   //Back sensor
   pinMode(echoPin1, INPUT);
   pinMode(trigPin1, OUTPUT);
+
   //Right sensor
   pinMode(echoPin2, INPUT);
   pinMode(trigPin2, OUTPUT);
+
   //Left sensor
   pinMode(echoPin3, INPUT);
   pinMode(trigPin3, OUTPUT);
@@ -60,7 +54,7 @@ int distance_check_back()
   Serial.print("cm ");
 }
 
-int distance_check_right()
+void distance_check_right()
 {
   digitalWrite(trigPin2, HIGH);
   delayMicroseconds(10);
@@ -72,7 +66,7 @@ int distance_check_right()
   Serial.print("cm ");
 }
 
-int distance_check_left()
+void distance_check_left()
 {
   digitalWrite(trigPin3, HIGH);
   delayMicroseconds(10);
@@ -85,63 +79,57 @@ int distance_check_left()
 }
 
 //Control the three possible cases (OK - GREEN, CAUTION - YELLOW and DANGER - RED)
-void green_case(int green){
-  digitalWrite(motor, HIGH);
-  digitalWrite(green, HIGH);
+void green_case(){
+  digitalWrite (RGB1, LOW);
+  digitalWrite (RGB2, LOW);
+  digitalWrite (RGB3, HIGH);
+}
+
+void yellow_case(){
+  digitalWrite (RGB1, 255);
+  digitalWrite (RGB2, LOW);
+  digitalWrite (RGB3, 50);
+}
+
+void red_case(){
+  digitalWrite (RGB1, HIGH);
+  digitalWrite (RGB2, LOW);
+  digitalWrite (RGB3, LOW);
+  tone (piezo, 1000, 5);
   delay(500);
-  digitalWrite(green, LOW);
 }
 
-void yellow_case(int yellow){
-  digitalWrite(yellow, HIGH);
-  delay(500);
-  digitalWrite(yellow, LOW);
-}
-
-void red_case(int red){
-  digitalWrite(motor, LOW);
-  digitalWrite(red, HIGH);
-  tone(piezo, 1000, 50);
-  delay(400);
-  digitalWrite(red, LOW);
-}
-
-// Functions to control movement possibilities
+//Functions to control movement possibilities
 void movement_control_back(){
- if (back_distance > 50){
-   green_case(greenled_back);
-
-  }else if (back_distance <= 50 and back_distance > 20){
-    yellow_case(yellowled_back);
-
-  }else{
-    red_case(redled_back);
-  }
-}
-
-void movement_control_right(){
- if (right_distance > 50){
-   green_case(A3);
-
-  }else if (right_distance <= 50 and right_distance > 20){
-    yellow_case(A4);
-
-  }else{
-    red_case(A5);
+ if (back_distance > 60){
+   green_case();
+ }else if (back_distance <= 60 and back_distance > 40){
+   yellow_case();
+ }else{
+   red_case();
   }
 }
 
 void movement_control_left(){
- if (left_distance > 50){
-   green_case(A0);
-
-  }else if (left_distance <= 50 and left_distance > 20){
-    yellow_case(A1);
-
-  }else{
-    red_case(A2);
+ if (left_distance > 60){
+   green_case();
+ }else if (left_distance <= 60 and left_distance > 40){
+   yellow_case();
+ }else{
+   red_case(); 
   }
 }
+
+void movement_control_right(){
+ if (right_distance > 60){
+   green_case();
+ }else if (right_distance <= 60 and right_distance > 40){
+   yellow_case();
+ }else{
+   red_case();
+  }
+}
+
 
 // Main function 
 void loop()
@@ -153,7 +141,7 @@ void loop()
   distance_check_right();
   movement_control_right();
   delay(120);
-
+  
   distance_check_left();
   movement_control_left();
   delay(120);
